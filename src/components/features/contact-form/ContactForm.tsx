3b750d6a64becdaf2c85d3d940e6d760a3e7c8d5
Gabriel Damalis <gabrieldamalis@gmail.com>
2025-02-08 00:10:05 -0300

    feat: New Contact Page (#3)
    * chore: update tailwindcss
    
    * feat: updated translations
    
    * feat: created Contact us page
    
    * chore: updated pull request template
    
    * fix: resolved errors and warnings

---
+++ src/components/features/contact-form/ContactForm.tsx
@@ -0,0 +1,140 @@
+import Image from 'next/image';
+
+import { Dropdown, type DropDownOption } from '@src/components/shared/dropdown';
+
+const DropDownOptions: DropDownOption[] = [
+  {
+    id: '1',
+    value: 'Petición de Oración',
+  },
+  {
+    id: '2',
+
+    value: 'Solicitud de Información',
+  },
+  {
+    id: '3',
+    value: 'Solicitud de Bautismo',
+  },
+  {
+    id: '5',
+    value: 'Solicitud de Consejería',
+  },
+  {
+    id: '6',
+    value: 'Solicitud de Ayuda',
+  },
+  {
+    id: '8',
+    value: 'Quier visitarlos',
+  },
+];
+
+export const ContactForm = () => {
+  return (
+    <div className="relative isolate bg-white px-6 py-20 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-xl lg:max-w-4xl">
+        <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+          Conectá con nosotros{' '}
+        </h2>
+        <p className="mt-4 text-lg/8 text-gray-600">
+          Podés escribirnos al correo{' '}
+          <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-lg font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+            idcredentor@gmail.com
+          </span>{' '}
+          o bien podés escribirnos a través de este breve formulario:
+        </p>
+        <div className="mt-8 flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
+          <form action="#" method="POST" className="lg:flex-auto">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="first-name"
+                    name="first-name"
+                    type="text"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
+                  Last name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="last-name"
+                    name="last-name"
+                    type="text"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="budget" className="block text-sm/6 font-semibold text-gray-900">
+                  Razón de contacto
+                </label>
+                <div className="mt-2.5">
+                  <Dropdown options={DropDownOptions} />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
+                  Mensaje
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows={4}
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                    defaultValue={''}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="mt-10">
+              <button
+                type="submit"
+                className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Let’s talk
+              </button>
+            </div>
+            <p className="mt-4 text-sm/6 text-gray-500">
+              Al enviar este formulario, aceptás la{' '}
+              <a href="/privacy-policyß" className="font-semibold text-indigo-600">
+                política de privacidad
+              </a>{' '}
+              .
+            </p>
+          </form>
+          <div className="lg:mt-6 lg:w-80 lg:flex-none">
+            <Image
+              src="/assets/img/redentor_logo.png"
+              className="h-24 w-24"
+              width={60}
+              height={80}
+              alt="Redentor church logo"
+            />
+            <figure className="mt-4">
+              <blockquote className="text-lg/8 font-semibold italic text-gray-900">
+                <p>&quot;Tú eres el Cristo, el Hijo del Dios viviente.&quot;</p>
+              </blockquote>
+              <figcaption className="mt-4 flex gap-x-6">
+                <div>
+                  <div className="text-base font-semibold text-gray-900">Mateo 16:16</div>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/features/contact-information-section/ContactInformationSection.tsx
@@ -0,0 +1,72 @@
+export const ContactInformationSection = () => {
+  return (
+    <div className="overflow-hidden bg-white py-20 sm:py-32">
+      <div className="mx-auto max-w-7xl md:px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:grid-cols-2 lg:items-start">
+          <div className="px-6 lg:px-0 lg:pr-4">
+            <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
+              <h2 className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                Horarios y Ubicación
+              </h2>
+              <div className="relative mt-6 flex items-center px-6 py-8 sm:px-10 sm:py-12">
+                <div className="absolute inset-0 overflow-hidden rounded-lg">
+                  {/* <img
+                    alt=""
+                    src="https://tailwindui.com/plus-assets/img/ecommerce-images/footer-02-exclusive-sale.jpg"
+                    className="size-full object-cover saturate-0 filter"
+                  /> */}
+                  <div className="absolute inset-0 bg-indigo-800/90" />
+                </div>
+                <div className="relative mx-auto max-w-sm text-center">
+                  <p className="text-2xl font-bold tracking-tight text-white">
+                    Domingos | 17:00 hs
+                  </p>
+                </div>
+              </div>
+              <p className="mt-2 text-sm italic text-gray-600">
+                Hay reuniones entre semana en diferentes barrios de la capital. Pueden solicitar más
+                información al correo o al formulario de contacto más abajo
+              </p>
+              <div className="relative mt-6 flex items-center px-6 py-12 sm:px-10 sm:py-16">
+                <div className="absolute inset-0 overflow-hidden rounded-lg">
+                  {/* <img
+                    alt=""
+                    src="https://tailwindui.com/plus-assets/img/ecommerce-images/footer-02-exclusive-sale.jpg"
+                    className="size-full object-cover saturate-0 filter"
+                  /> */}
+                  <div className="absolute inset-0 bg-indigo-600/90" />
+                </div>
+                <div className="relative mx-auto max-w-sm text-center">
+                  <p className="text-2xl font-bold tracking-tight text-white">
+                    Tte. Gral. Juan Domingo Perón 4385
+                  </p>
+                  <p className="text-2xl tracking-tight text-white">
+                    Almargo, CABA, República Argentina
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="sm:px-6 lg:px-0">
+            <div className="relative isolate overflow-hidden sm:mx-auto sm:max-w-2xl sm:rounded-3xl lg:mx-0 lg:max-w-none">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d205.24589880502776!2d-58.428660275340235!3d-34.60582077587076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccba90ef56ead%3A0x76e96b1ea8cfa41!2sIglesia%20de%20Cristo%20Parque%20Centenario!5e0!3m2!1sen!2sar!4v1738971838553!5m2!1sen!2sar"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                title='Mapa de la iglesia "Iglesia de Cristo"'
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+              <div
+                aria-hidden="true"
+                className="pointer-events-none absolute inset-0 ring-1 ring-inset ring-black/10 sm:rounded-3xl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
