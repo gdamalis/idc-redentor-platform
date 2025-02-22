757063563f457b7bc6e3c8f6a79a104885a799ef
Gabriel Damalis <gabrieldamalis@gmail.com>
2025-02-22 15:01:53 -0300

    chore: removed ignore file and add to config file the ignored paths

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { ignores: ["scripts/**/*.js", "src/**/__generated"] },
 ];
 
 export default eslintConfig;
