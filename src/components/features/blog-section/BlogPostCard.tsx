380d4018db26ede68fb04f550cfca7c059d30eb3
Gabriel Damalis <gabrieldamalis@gmail.com>
2025-04-12 03:06:33 -0300

    feat: introduce BlogPostCard component for modular blog post display
    - Created a new `BlogPostCard` component to encapsulate the rendering of individual blog posts, enhancing modularity and readability.
    - Updated `BlogSection` to utilize the new `BlogPostCard` component, streamlining the layout and improving maintainability.
    - Removed inline rendering logic from `BlogSection`, promoting cleaner code structure.

---
+++ src/components/features/blog-section/BlogPostCard.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import { useLocale } from "next-intl";
+
+import { Typography } from "@src/components/ui/typography";
+import { Link } from "@src/i18n/routing";
+import { BlogPost } from "@src/types/BlogPost";
+import { formatDate } from "@src/utils/formatDate";
+
+type BlogPostCardProps = {
+  post: BlogPost;
+};
+
+export const BlogPostCard = ({ post }: BlogPostCardProps) => {
+  const locale = useLocale();
+  const formattedDate = formatDate(post.publishedDate, locale);
+
+  return (
+    <article
+      key={post.sys.id}
+      className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
+    >
+      <Image
+        alt={post.featuredImage.title}
+        src={post.featuredImage.url}
+        width={780}
+        height={780}
+        className="absolute inset-0 -z-10 h-full w-full object-cover"
+      />
+      <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
+      <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+
+      <div className="flex flex-wrap items-center gap-y-1 gap-x-3 overflow-hidden text-sm leading-6 text-gray-300">
+        <time dateTime={post.publishedDate} className="">
+          {formattedDate}
+        </time>
+        <svg
+          viewBox="0 0 2 2"
+          className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50"
+        >
+          <circle r={1} cx={1} cy={1} />
+        </svg>
+        <div className="flex gap-x-2.5">
+          <Image
+            alt={post.author.avatar.title}
+            src={post.author.avatar.url}
+            width={24}
+            height={24}
+            className="h-6 w-6 flex-none rounded-full bg-white/10"
+          />
+          {post.author.name}
+        </div>
+      </div>
+      <Typography
+        component="h3"
+        variant="h3"
+        className="mt-3 text-lg font-semibold leading-6 text-white"
+      >
+        <Link href={`/blog/${post.slug}`}>
+          <span className="absolute inset-0" />
+          {post.title}
+        </Link>
+      </Typography>
+    </article>
+  );
+};
--- src/components/features/blog-section/BlogSection.tsx
+++ src/components/features/blog-section/BlogSection.tsx
@@ -1,16 +1,14 @@
 import { Typography } from "@src/components/ui/typography";
-import { Link } from "@src/i18n/routing";
-import { useLocale, useTranslations } from "next-intl";
-import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 import { BlogPost } from "@src/types/BlogPost";
+import { BlogPostCard } from "./BlogPostCard";
 
 type BlogSectionProps = {
   posts: BlogPost[];
 };
 
 export const BlogSection = ({ posts }: BlogSectionProps) => {
-  const locale = useLocale();
   const t = useTranslations("Blog");
 
   return (
@@ -34,59 +32,7 @@ export const BlogSection = ({ posts }: BlogSectionProps) => {
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
-            <article
-              key={post.sys.id}
-              className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
-            >
-              <Image
-                alt={post.featuredImage.title}
-                src={post.featuredImage.url}
-                width={780}
-                height={780}
-                className="absolute inset-0 -z-10 h-full w-full object-cover"
-              />
-              <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
-              <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-
-              <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-                <time dateTime={post.publishedDate} className="mr-8">
-                  {new Date(post.publishedDate)
-                    .toLocaleDateString(locale, {
-                      day: "2-digit",
-                      month: "2-digit",
-                      year: "numeric",
-                    })}
-                </time>
-                <div className="-ml-4 flex items-center gap-x-4">
-                  <svg
-                    viewBox="0 0 2 2"
-                    className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50"
-                  >
-                    <circle r={1} cx={1} cy={1} />
-                  </svg>
-                  <div className="flex gap-x-2.5">
-                    <Image
-                      alt={post.author.avatar.title}
-                      src={post.author.avatar.url}
-                      width={24}
-                      height={24}
-                      className="h-6 w-6 flex-none rounded-full bg-white/10"
-                    />
-                    {post.author.name}
-                  </div>
-                </div>
-              </div>
-              <Typography
-                component="h3"
-                variant="h3"
-                className="mt-3 text-lg font-semibold leading-6 text-white"
-              >
-                <Link href={`/blog/${post.slug}`}>
-                  <span className="absolute inset-0" />
-                  {post.title}
-                </Link>
-              </Typography>
-            </article>
+            <BlogPostCard key={post.sys.id} post={post} />
           ))}
         </div>
       </div>
