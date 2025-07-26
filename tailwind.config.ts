0f2da7c18f0ff3122f7c328f0c7202972c4107de
Gabriel Damalis <gabrieldamalis@gmail.com>
2025-07-26 17:24:50 -0300

    chore: update Tailwind CSS configuration and refactor global styles
    - Changed Tailwind CSS plugin reference in postcss.config.mjs to "@tailwindcss/postcss".
    - Refactored tailwind.config.ts by removing unused theme extensions and retaining only the typography plugin.
    - Updated globals.css to use CSS variables for theming and added keyframes for animations.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 import typography from "@tailwindcss/typography";
 
 export default {
@@ -8,42 +7,5 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      backgroundImage: {
-        community: "url('/assets/img/community_redentor_camp.jpeg')",
-      },
-      maxWidth: {
-        "8xl": "90rem",
-      },
-      letterSpacing: {
-        snug: "-0.011em",
-      },
-      fontSize: {
-        "2xs": "0.625rem",
-        "3xl": "1.75rem",
-        "4xl": "2.5rem",
-      },
-      lineHeight: {
-        tighter: "1.1",
-      },
-      fontFamily: {
-        sans: ["var(--font-nunito-sans)", ...fontFamily.sans],
-      },
-      keyframes: {
-        highlight: {
-          from: { "background-size": "0% 100%" },
-          to: { "background-size": "100% 100%" },
-        },
-      },
-      animation: {
-        highlight: "highlight 1s ease-in-out forwards",
-      },
-    },
-  },
   plugins: [typography],
 } satisfies Config;
