0e880f82d1d845c6480338ace15bc8930ce74521
Gabriel Damalis <gabrieldamalis@gmail.com>
2025-11-28 19:29:41 -0300

    style: enhance input and textarea components with improved focus ring
    - Updated the focus ring styling in Input and Textarea components from ring-1 to ring-2 for better visibility.
    - Adjusted Dropdown component styles for consistency, including outline and focus states.
    - Improved class names for clarity and alignment with Tailwind CSS best practices.

--- src/components/ui/dropdown/Dropdown.tsx
+++ src/components/ui/dropdown/Dropdown.tsx
@@ -40,7 +40,7 @@ export const Dropdown = ({ options, placeholder, name, id }: DropdownProps) => {
           />
         )}
 
-        <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md  py-2 pl-3 pr-2 text-left text-gray-900 dark:text-gray-100 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-primary sm:text-sm/6 dark:bg-gray-950">
+        <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md  py-2 pl-3 pr-2 text-left text-gray-900 dark:text-gray-100 outline -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-primary sm:text-sm/6 dark:bg-gray-950">
           <span
             className={`col-start-1 row-start-1 truncate pr-6 ${placeholderStyle}`}
           >
@@ -54,19 +54,19 @@ export const Dropdown = ({ options, placeholder, name, id }: DropdownProps) => {
 
         <ListboxOptions
           transition
-          className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md  border border-gray-300 dark:border-gray-700 py-1 text-base shadow-lg ring-1 ring-gray-700/5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm dark:bg-gray-950"
+          className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white border border-gray-300 dark:border-gray-700 py-1 text-base shadow-lg ring-1 ring-gray-700/5 focus:outline-none data-closed:data-leave:opacity-0 data-leave:transition data-leave:duration-100 data-leave:ease-in sm:text-sm"
         >
           {options.map((option) => (
             <ListboxOption
               key={option.id}
               value={option}
-              className="group relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900 dark:text-gray-100 data-[focus]:bg-primary data-[focus]:text-white data-[focus]:outline-none"
+              className="group relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900 dark:text-gray-100 data-focus:bg-primary data-focus:text-white data-focus:outline-none"
             >
-              <span className="block truncate font-normal group-data-[selected]:font-semibold">
+              <span className="block truncate font-normal group-data-selected:font-semibold">
                 {option.value}
               </span>
 
-              <span className="absolute inset-y-0 left-0 flex items-center pl-1.5 text-primary dark:text-gray-100 group-[&:not([data-selected])]:hidden group-data-[focus]:text-white">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-1.5 text-primary dark:text-gray-100 group-[&:not([data-selected])]:hidden group-data-focus:text-white">
                 <CheckIcon aria-hidden="true" className="size-5" />
               </span>
             </ListboxOption>
